703e57ec013244ec7720d65776706729db34900f
Caleb Cushing <xenoterracide@gmail.com>
2011-08-12 18:39:29 -0500

    add Capture response stub
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

---
+++ lib/Business/CyberSource/Response/Capture.pm
@@ -0,0 +1,12 @@
+package Business::CyberSource::Response::Authorization;
+use 5.008;
+use strict;
+use warnings;
+BEGIN {
+	# VERSION
+}
+use Moose;
+with 'Business::CyberSource::Response';
+
+__PACKAGE__->meta->make_immutable;
+1;
