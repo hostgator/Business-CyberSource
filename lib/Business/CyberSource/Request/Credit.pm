e55cc9af3c632146866104afc3e2d1598bbe7608
Caleb Cushing <xenoterracide@gmail.com>
2011-08-15 15:56:17 -0500

    add data to _build_sdbo in credit request
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Credit.pm
+++ lib/Business/CyberSource/Request/Credit.pm
@@ -26,12 +26,20 @@ sub _build_sdbo {
 	my $self = shift;
 
 	my $sb = $self->_build_sdbo_header;
+	$sb = $self->_build_bill_to_info    ( $sb );
+	$sb = $self->_build_purchase_info   ( $sb );
+	$sb = $self->_build_credit_card_info( $sb );
+
+	$sb->add_elem(
+		attributes => { run => 'true' },
+		name       => 'ccCreditService',
+		value      => ' ', # hack to prevent cs side unparseable xml
+	);
 
 	return $sb;
 }
 
 __PACKAGE__->meta->make_immutable;
-
 1;
 
 # ABSTRACT: CyberSource Credit Request Object
