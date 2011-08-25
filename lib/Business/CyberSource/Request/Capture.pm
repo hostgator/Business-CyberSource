3b1a6da014f750012fe986393208de4468f8faaa
Caleb Cushing <xenoterracide@gmail.com>
2011-08-25 18:21:42 -0500

    flesh out documentation for capture requests
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Capture.pm
+++ lib/Business/CyberSource/Request/Capture.pm
@@ -103,7 +103,30 @@ __PACKAGE__->meta->make_immutable;
 
 =head1 SYNOPSIS
 
+	my $capture = Business::CyberSource::Request::Capture->new({
+		username       => 'merchantID',
+		password       => 'transaction key',
+		production     => 0,
+		reference_code => 'merchant reference code',
+		request_id     => 'authorization response request_id',
+		total          => 5.01,  # same amount as in authorization
+		currency       => 'USD', # same currency as in authorization
+	});
+
 =head1 DESCRIPTION
+
+This object allows you to create a request for a capture.
+
+=method new
+
+Instantiates a authorization reversal request object, see
+L<the attributes listed below|/ATTRIBUTES> for which ones are required and
+which are optional.
+
+=method submit
+
+Actually sends the required data to CyberSource for processing and returns a
+L<Business::CyberSource::Response> object.
 
 =head1 SEE ALSO
 
