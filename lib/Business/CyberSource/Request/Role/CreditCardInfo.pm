99c5cdcd292cfccf6476efc26501184f8d6e7f1c
Caleb Cushing <xenoterracide@gmail.com>
2011-08-26 01:41:30 -0500

    add cvIndicator if cvn sent
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Role/CreditCardInfo.pm
+++ lib/Business/CyberSource/Request/Role/CreditCardInfo.pm
@@ -10,6 +10,7 @@ use Moose::Role;
 use namespace::autoclean;
 use MooseX::Aliases;
 use MooseX::Types::Moose      qw( Int        );
+use MooseX::Types::Varchar    qw( Varchar    );
 use MooseX::Types::CreditCard qw( CreditCard );
 
 has credit_card => (
@@ -35,6 +36,15 @@ has cc_exp_year => (
 	required => 1,
 	is       => 'ro',
 	isa      => Int,
+);
+
+has cv_indicator => (
+	required => 0,
+	lazy     => 1,
+	is       => 'ro',
+	isa      => Varchar[1],
+	default  => '1',
+	documentation => 'Flag that indicates whether a CVN code was sent'
 );
 
 has cvn => (
@@ -73,6 +83,12 @@ sub _build_credit_card_info {
 		$sb->add_elem(
 			name   => 'cvNumber',
 			value  => $self->cvn,
+			parent => $card,
+		);
+
+		$sb->add_elem(
+			name   => 'cvIndicator',
+			value  => $self->cv_indicator,
 			parent => $card,
 		);
 	}
