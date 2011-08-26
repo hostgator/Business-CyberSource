84e507ff2821727cf54349572bf389bd932f591e
Caleb Cushing <xenoterracide@gmail.com>
2011-08-26 00:52:19 -0500

    validate field length in billing info
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Role/BillingInfo.pm
+++ lib/Business/CyberSource/Request/Role/BillingInfo.pm
@@ -3,45 +3,49 @@ use 5.008;
 use strict;
 use warnings;
 use Carp;
-BEGIN {
-	# VERSION
-}
+
+# VERSION
+
 use Moose::Role;
-use MooseX::Types::Email qw( EmailAddress );
+use namespace::autoclean;
+use MooseX::Aliases;
+use MooseX::Types::Varchar         qw( Varchar       );
+use MooseX::Types::Email           qw( EmailAddress  );
 use MooseX::Types::Locale::Country qw( Alpha2Country );
 
 has first_name => (
 	required => 1,
 	is       => 'ro',
-	isa      => 'Str',
+	isa      => Varchar[60],
 	documentation => 'Card Holder\'s first name',
 );
 
 has last_name => (
 	required => 1,
 	is       => 'ro',
-	isa      => 'Str',
+	isa      => Varchar[60],
 	documentation => 'Card Holder\'s last name',
 );
 
 has street => (
 	required => 1,
 	is       => 'ro',
-	isa      => 'Str',
+	isa      => Varchar[60],
+	alias    => 'street1',
 	documentation => 'Street address on credit card billing statement',
 );
 
 has city => (
 	required => 1,
 	is       => 'ro',
-	isa      => 'Str',
+	isa      => Varchar[50],
 	documentation => 'City on credit card billing statement',
 );
 
 has state => (
 	required => 1,
 	is       => 'ro',
-	isa      => 'Str',
+	isa      => Varchar[2],
 	documentation => 'State on credit card billing statement',
 );
 
@@ -57,7 +61,7 @@ has country => (
 has zip => (
 	required => 1,
 	is       => 'ro',
-	isa      => 'Str',
+	isa      => Varchar[10],
 	documentation => 'postal code on credit card billing statement',
 );
 
@@ -70,7 +74,7 @@ has email => (
 has ip => (
 	required => 0,
 	is       => 'ro',
-	isa      => 'Str',
+	isa      => Varchar[15],
 	documentation => 'IP address that customer submitted transaction from',
 );
 
