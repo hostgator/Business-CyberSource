7b3db4cf53b78c46c13031e74a1760dcc230e7d1
Caleb Cushing <xenoterracide@gmail.com>
2011-08-26 01:24:38 -0500

    make client info not required
    it will be initialized anyways
    
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource.pm
+++ lib/Business/CyberSource.pm
@@ -3,14 +3,14 @@ use 5.006;
 use strict;
 use warnings;
 use Carp;
-BEGIN {
-	# VERSION
-}
+
+# VERSION
+
 use Moose::Role;
 use namespace::autoclean;
 
 has client_version => (
-	required => 1,
+	required => 0,
 	lazy     => 1,
 	init_arg => undef,
 	is       => 'ro',
@@ -19,7 +19,7 @@ has client_version => (
 );
 
 has client_name => (
-	required => 1,
+	required => 0,
 	lazy     => 1,
 	init_arg => undef,
 	is       => 'ro',
@@ -28,7 +28,7 @@ has client_name => (
 );
 
 has client_env => (
-	required => 1,
+	required => 0,
 	lazy     => 1,
 	init_arg => undef,
 	is       => 'ro',
