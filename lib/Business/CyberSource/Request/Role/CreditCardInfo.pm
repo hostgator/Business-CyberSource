d53738d82bf149b5cdd711a47a6ca50b5b47a5ee
Caleb Cushing <xenoterracide@gmail.com>
2011-09-01 03:03:59 -0500

    remove further sdbo methods
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Role/CreditCardInfo.pm
+++ lib/Business/CyberSource/Request/Role/CreditCardInfo.pm
@@ -54,48 +54,6 @@ has cvn => (
 	isa      => CardSecurityCode,
 );
 
-sub _build_credit_card_info {
-	my ( $self, $sb ) = @_;
-
-	my $card = $sb->add_elem(
-		name => 'card',
-	);
-
-	$sb->add_elem(
-		name   => 'accountNumber',
-		value  => $self->credit_card,
-		parent => $card,
-	);
-
-	$sb->add_elem(
-		name   => 'expirationMonth',
-		value  => $self->cc_exp_month,
-		parent => $card,
-	);
-
-	$sb->add_elem(
-		name   => 'expirationYear',
-		value  => $self->cc_exp_year,
-		parent => $card,
-	);
-
-	if ( $self->cvn ) {
-		$sb->add_elem(
-			name   => 'cvIndicator',
-			value  => $self->cv_indicator,
-			parent => $card,
-		);
-
-		$sb->add_elem(
-			name   => 'cvNumber',
-			value  => $self->cvn,
-			parent => $card,
-		);
-	}
-
-	return $sb;
-}
-
 1;
 
 # ABSTRACT: credit card info role
