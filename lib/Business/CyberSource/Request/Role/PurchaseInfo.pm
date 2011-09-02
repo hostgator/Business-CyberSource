4141409fcb6b73bd33fbb42047ffb71993d8fe69
Caleb Cushing <xenoterracide@gmail.com>
2011-09-02 04:31:30 -0500

    add purchase_info method
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Role/PurchaseInfo.pm
+++ lib/Business/CyberSource/Request/Role/PurchaseInfo.pm
@@ -11,6 +11,21 @@ use MooseX::Types::Moose   qw( Num     );
 use MooseX::Types::Varchar qw( Varchar );
 use MooseX::Types::Locale::Currency qw( CurrencyCode );
 
+sub _purchase_info {
+	my $self = shift;
+
+	my $i = {
+		currency         => $self->currency,
+		grandTotalAmount => $self->total,
+	}
+
+	if ( $self->foreign_currency ) {
+		$i->{foreignCurrency} = $self->foreign_currency;
+	}
+
+	return $i;
+}
+
 has currency => (
 	required => 1,
 	is       => 'ro',
