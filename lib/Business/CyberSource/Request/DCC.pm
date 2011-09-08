925ea9f24eb4cf33d2355bc3b9cf204b79ab0b46
Caleb Cushing <xenoterracide@gmail.com>
2011-09-07 20:20:41 -0500

    spelling correction
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/DCC.pm
+++ lib/Business/CyberSource/Request/DCC.pm
@@ -63,7 +63,7 @@ __PACKAGE__->meta->make_immutable;
 =head1 DESCRIPTION
 
 This object allows you to create a request for Direct Currency Conversion.
-This object is not known to work correctly. Although it follow's the
+This object is not known to work correctly. Although it follows the
 CyberSource Documentation for DCC request, the response returned appears to
 always be a 150 General Error.
 
