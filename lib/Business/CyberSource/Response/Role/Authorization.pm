241da178f153c7c63a32548f5ad50745bbebb860
Caleb Cushing <xenoterracide@gmail.com>
2011-09-07 21:54:04 -0500

    mostly tighten down attributes per CS documentation
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Response/Role/Authorization.pm
+++ lib/Business/CyberSource/Response/Role/Authorization.pm
@@ -2,15 +2,16 @@ package Business::CyberSource::Response::Role::Authorization;
 use 5.008;
 use strict;
 use warnings;
-BEGIN {
-	# VERSION
-}
+
+# VERSION
+
 use Moose::Role;
+use MooseX::Types::Varchar qw( Varchar );
 
 has auth_code => (
 	required => 1,
 	is       => 'ro',
-	isa      => 'Num',
+	isa      => Varchar[7],
 );
 
 has auth_record => (
@@ -22,21 +23,24 @@ has auth_record => (
 has avs_code => (
 	required => 1,
 	is       => 'ro',
-	isa      => 'Str',
+	isa      => Varchar[1],
+	documentation => 'AVS results.',
 );
 
 has avs_code_raw => (
-	required => 1,
+	required => 0,
 	is       => 'ro',
-	isa      => 'Str',
+	isa      => Varchar[10],
+	documentation => 'AVS result code sent directly from the processor. '
+		. 'Returned only if a value is returned by the processor.',
 );
 
 has processor_response => (
 	required => 1,
 	is       => 'ro',
-	isa      => 'Str',
+	isa      => Varchar[10],
 );
 
 1;
 
-# ABSTRACT: CyberSource Authorization Response object
+# ABSTRACT: CyberSource Authorization Response only attributes
