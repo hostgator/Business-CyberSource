2362ee43851bca5af18d94e30a2dc5f2709d517e
Caleb Cushing <xenoterracide@gmail.com>
2011-10-05 23:56:07 -0500

    add a synopsis to dcc
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/DCC.pm
+++ lib/Business/CyberSource/Request/DCC.pm
@@ -66,6 +66,87 @@ __PACKAGE__->meta->make_immutable;
 
 # ABSTRACT: CyberSource DCC Request Object
 
+=head1 SYNOPSIS
+
+	my $CYBS_ID = 'myMerchantID';
+	my $CYBS_KEY = 'transaction key generated with cybersource';
+
+	use Business::CyberSource::Request;
+
+	my $factory
+		= Business::CyberSource::Request->new({
+			username       => $CYBS_ID,
+			password       => $CYBS_KEY,
+			production     => 0,
+		});
+
+	my $dcc_req = $factory->create( 'DCC',
+		{
+			reference_code => '1984',
+			currency       => 'USD',
+			credit_card    => '5100870000000004',
+			cc_exp_month   => '04',
+			cc_exp_year    => '2012',
+			total          => '1.00',
+			foreign_currency => 'EUR',
+		});
+
+	my $dcc_res = $dcc_req->submit;
+
+	my $auth_req = $factory->create( 'Authorization',
+		{
+			reference_code   => '1984',
+			first_name       => 'Caleb',
+			last_name        => 'Cushing',
+			street           => 'somewhere',
+			city             => 'Houston',
+			state            => 'TX',
+			zip              => '77064',
+			country          => 'US',
+			email            => 'xenoterracide@gmail.com',
+			credit_card      => '5100870000000004',
+			total            => '1.00',
+			currency         => 'USD',
+		 	foreign_currency => 'EUR',
+			foreign_amount   => $dcc_res->foreign_amount,
+			exchange_rate    => $dcc_res->exchange_rate,
+			cc_exp_month     => '04',
+			cc_exp_year      => '2012',
+			dcc_indicator    => 1,
+			exchange_rate_timestamp => $dcc_res->exchange_rate_timestamp,
+		});
+
+	my $auth_res = $auth_req->submit;
+
+	my $cap_req = $factory->create( 'Capture',
+		{
+			reference_code   => '1984',
+			total            => '1.00'
+			currency         => 'USD'
+			foreign_currency => 'EUR'
+			foreign_amount   => $dcc_res->foreign_amount,
+			exchange_rate    => $dcc_res->exchange_rate,
+			dcc_indicator    => 1,
+			request_id       => $auth_res->request_id,
+			exchange_rate_timestamp => $dcc_res->exchange_rate_timestamp,
+		});
+
+	my $cap_res = $cap_req->submit;
+
+	my $cred_req = $factory->create( 'FollowOnCredit',
+		{
+			reference_code   => '1984',
+			total            => '1.00'
+			currency         => 'USD'
+			foreign_currency => 'EUR'
+			foreign_currency => $dcc_res->foreign_currency,
+			foreign_amount   => $dcc_res->foreign_amount,
+			exchange_rate    => $dcc_res->exchange_rate,
+			dcc_indicator    => 1,
+			request_id       => $cap_res->request_id,
+			exchange_rate_timestamp => $dcc_res->exchange_rate_timestamp,
+		})
+
 =head1 DESCRIPTION
 
 This object allows you to create a request for Direct Currency Conversion.
