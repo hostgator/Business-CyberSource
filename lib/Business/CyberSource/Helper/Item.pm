61c326b4135d34e6ebc923a5f4886afe8eeca0cf
Caleb Cushing <xenoterracide@gmail.com>
2012-05-25 19:12:07 -0500

    add and test helper item
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

---
+++ lib/Business/CyberSource/Helper/Item.pm
@@ -0,0 +1,91 @@
+package Business::CyberSource::Helper::Item;
+use strict;
+use warnings;
+use namespace::autoclean;
+
+# VERSION
+
+use Moose;
+with 'MooseX::RemoteHelper::CompositeSerialization';
+
+use MooseX::RemoteHelper;
+use MooseX::SetOnce;
+
+use MooseX::Types::Common::Numeric qw( PositiveOrZeroNum PositiveOrZeroInt );
+use MooseX::Types::Common::String  qw( NonEmptySimpleStr );
+
+has id => (
+	isa         => PositiveOrZeroInt,
+	remote_name => 'id',
+	is          => 'ro',
+	required    => 1,
+);
+
+has quantity => (
+	isa         => PositiveOrZeroInt,
+	remote_name => 'quantity',
+	is          => 'ro',
+	lazy        => 1,
+	default     => sub { 1 },
+);
+
+has unit_price => (
+	isa         => PositiveOrZeroNum,
+	remote_name => 'unitPrice',
+	is          => 'ro',
+	required    => 1,
+);
+
+has product_code => (
+	isa         => NonEmptySimpleStr,
+	remote_name => 'productCode',
+	traits      => ['SetOnce'],
+	is          => 'rw',
+);
+
+has product_name => (
+	isa         => NonEmptySimpleStr,
+	remote_name => 'productName',
+	traits      => ['SetOnce'],
+	is          => 'rw',
+);
+
+has product_sku => (
+	isa         => NonEmptySimpleStr,
+	remote_name => 'productSKU',
+	traits      => ['SetOnce'],
+	is          => 'rw',
+);
+
+has product_risk => (
+	isa         => NonEmptySimpleStr,
+	remote_name => 'productRisk',
+	traits      => ['SetOnce'],
+	is          => 'rw',
+);
+
+has tax_amount => (
+	isa         => PositiveOrZeroNum,
+	remote_name => 'taxAmount',
+	traits      => ['SetOnce'],
+	is          => 'rw',
+);
+
+has tax_rate => (
+	isa         => PositiveOrZeroNum,
+	remote_name => 'taxRate',
+	traits      => ['SetOnce'],
+	is          => 'rw',
+);
+	
+has national_tax => (
+	isa         => PositiveOrZeroNum,
+	remote_name => 'nationalTax',
+	traits      => ['SetOnce'],
+	is          => 'rw',
+);
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+# ABSTRACT: Item Helper Class
