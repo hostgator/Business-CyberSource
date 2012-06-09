2a11534199630a50e81f02efcb100e2a96e12b46
Caleb Cushing <xenoterracide@gmail.com>
2012-06-08 21:16:07 -0500

    update BusinessRules documentation
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/RequestPart/BusinessRules.pm
+++ lib/Business/CyberSource/RequestPart/BusinessRules.pm
@@ -90,7 +90,48 @@ __PACKAGE__->meta->make_immutable;
 
 =attr ignore_avs_result
 
+Boolean that indicates whether to allow C<ccCaptureService> to run even when the
+authorization receives an AVS decline. Used only when both C<ccAuthService> and
+C<ccCaptureService> are requested at the same time.
+Possible values:
+
+=over
+
+=item true
+
+Ignore the results of AVS checking and run the C<ccCaptureService> service.
+
+=item false (default on remote)
+
+If the authorization receives an AVS decline, do not run the
+C<ccCaptureService> service.
+
+=back
+
+If the value of this field is true, the list in the
+businessRules_declineAVSFlags field is ignored.
+
 =attr ignore_cv_result
+
+Flag that indicates whether to allow C<ccCaptureService> to run if the value of
+the reply field C<ccAuthReply_cvCode> is D or N. Used only when both
+C<ccAuthService>
+and C<ccCaptureService> are requested at the same time.
+Possible values:
+
+=over
+
+=item true
+
+If the value of C<ccAuthReply_cvCode> is D or N, allow C<ccCaptureService> to
+run.
+
+=item false (default on remote)
+
+If the value of C<ccAuthReply_cvCode> is D or N, return a card
+verification decline and do not allow C<ccCaptureService> to run.
+
+=back
 
 =attr ignore_dav_result
 
@@ -99,5 +140,10 @@ __PACKAGE__->meta->make_immutable;
 =attr ignore_validate_result
 
 =attr decline_avs_flags
+
+ArrayRef of AVS flags that cause the request to be declined for AVS reasons.
+
+B<Important> Make sure that you include the value N in the list if you want to
+receive declines for the AVS code N.
 
 =attr score_threshold
