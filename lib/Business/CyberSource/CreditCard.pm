a5d4c3277d4c301f07b0baab1afd82650bc01e25
Caleb Cushing <xenoterracide@gmail.com>
2012-06-08 21:23:41 -0500

    relocate documentation
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/CreditCard.pm
+++ lib/Business/CyberSource/CreditCard.pm
@@ -1,5 +1,4 @@
 package Business::CyberSource::CreditCard;
-use 5.010;
 use strict;
 use warnings;
 use namespace::autoclean;
@@ -17,42 +16,5 @@ __PACKAGE__->meta->make_immutable;
 =head1 DESCRIPTION
 
 Just a L<Business::CyberSource::RequestPart::Card>, use that instead.
-
-=attr account_number
-
-This is the Credit Card Number
-
-=attr type
-
-The card issuer, e.g. VISA, MasterCard. it is generated from the card number.
-
-=attr expiration
-
-	my $card = Business::CyberSource::CreditCard->new({
-			account_number => '4111111111111111',
-			expiration     => {
-				year  => '2025',
-				month => '04',
-			},
-		});
-
-A DateTime object, you should construct it by passing a hashref with keys for
-month, and year, it will actually contain the last day of that month/year.
-
-=attr is_expired
-
-Boolean, returns true if the card is older than
-L<expiration date|/"expiration"> plus one day. This is done to compensate for
-unknown issuer time zones as we can't be sure that all issuers shut cards of on
-the first of every month UTC. In fact I have been told that some issuers will
-allow renewed cards to be run with expired dates. Use this at your discretion.
-
-=attr security_code
-
-The 3 digit security number on the back of the card.
-
-=attr holder
-
-The full name of the card holder as printed on the card.
 
 =cut
--- lib/Business/CyberSource/RequestPart/Card.pm
+++ lib/Business/CyberSource/RequestPart/Card.pm
@@ -194,3 +194,41 @@ __PACKAGE__->meta->make_immutable;
 
 L<Business::CyberSource::MessagePart>
 
+=attr account_number
+
+This is the Credit Card Number
+
+=attr type
+
+The card issuer, e.g. VISA, MasterCard. it is generated from the card number.
+
+=attr expiration
+
+	my $card = Business::CyberSource::CreditCard->new({
+			account_number => '4111111111111111',
+			expiration     => {
+				year  => '2025',
+				month => '04',
+			},
+		});
+
+A DateTime object, you should construct it by passing a hashref with keys for
+month, and year, it will actually contain the last day of that month/year.
+
+=attr is_expired
+
+Boolean, returns true if the card is older than
+L<expiration date|/"expiration"> plus one day. This is done to compensate for
+unknown issuer time zones as we can't be sure that all issuers shut cards of on
+the first of every month UTC. In fact I have been told that some issuers will
+allow renewed cards to be run with expired dates. Use this at your discretion.
+
+=attr security_code
+
+The 3 digit security number on the back of the card.
+
+=attr holder
+
+The full name of the card holder as printed on the card.
+
+=cut
