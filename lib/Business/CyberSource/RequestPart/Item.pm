5be77f7535136808e3d0093bd614ada5fe93857b
Caleb Cushing <xenoterracide@gmail.com>
2012-06-08 23:26:40 -0500

    document an item
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/RequestPart/Item.pm
+++ lib/Business/CyberSource/RequestPart/Item.pm
@@ -83,3 +83,57 @@ __PACKAGE__->meta->make_immutable;
 =head1 EXTENDS
 
 L<Business::CyberSource::MessagePart>
+
+=attr unit_price
+
+Per-item price of the product. You must include either this field or
+L<total|Business::CyberSource::RequestPart::PurchaseTotals/"total">
+in your request.
+
+=attr quantity
+
+The default is 1. For C<ccAuthService> and C<ccCaptureService>, this field is
+required if L<product_code|/"product_code> is not default or one of the values related to
+shipping and/or handling.
+
+=attr product_code
+
+Type of product. This value is used to determine the category that the product
+is in.
+
+=attr product_name
+
+For C<ccAuthService> and C<ccCaptureService>, this field is required if
+L<product_code|/product_code> is not default or one of the values related to shipping
+and/or handling.
+
+=attr product_sku
+
+Identification code for the product. For C<ccAuthService> and
+C<ccCaptureService>, this field is required if L<product_code|/product_code>
+is not default or one of the values related to shipping and/or handling.
+
+=attr product_risk
+
+=attr tax_amount
+
+Total tax to apply to the product. This value cannot be negative. The tax
+amount and the offer amount must be in the same currency.
+The tax amount field is additive. For example (in CyberSource notation):
+
+You include the following offer lines in your request:
+
+	item_0_unitPrice=10.00
+	item_0_quantity=1
+	item_0_taxAmount=0.80
+	item_1_unitPrice=20.00
+	item_1_quantity=1
+	item_1_taxAmount=1.60
+
+The total amount authorized will be 32.40, not 30.00 with 2.40 of tax included.
+
+=attr tax_rate
+
+=attr national_tax
+
+=cut
