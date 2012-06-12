cd61d2fb4a5aceae57ae85fc9d7ee07f68e5c2af
Caleb Cushing <xenoterracide@gmail.com>
2012-06-12 18:14:34 -0500

    add RequestIDisZero to rules
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

---
+++ lib/Business/CyberSource/Rule/RequestIDisZero.pm
@@ -0,0 +1,39 @@
+package Business::CyberSource::Rule::RequestIDisZero;
+use strict;
+use warnings;
+use namespace::autoclean;
+
+# VERSION
+
+use Moose;
+extends 'Business::CyberSource::Rule';
+
+sub run {
+	my ( $self, $request ) = @_;
+
+	# if the request can card it's required so no need to check has
+	return unless $request->service->can('request_id')
+		&& $request->service->has_request_id
+		&& $request->service->request_id == 0
+		;
+
+	$self->debug if $self->client->debug;
+
+	return { result => {
+		merchantReferenceCode => $request->reference_code,
+		decision              => 'REJECT',
+		reasonCode            => '241',
+		requestID             => 0,
+		requestToken          => 0,
+	}};
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
+# ABSTRACT: Expired Card
+
+=method run
+
+returns a 241 REJECT: The request ID is invalid
+
+=cut
