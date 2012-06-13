174550e8a08396df8409d5b08d89475ec5a93a6a
Caleb Cushing <xenoterracide@gmail.com>
2012-06-13 00:16:31 -0500

    add preliminary support for tax service
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Capture.pm
+++ lib/Business/CyberSource/Request/Capture.pm
@@ -7,7 +7,10 @@ use namespace::autoclean;
 
 use Moose;
 extends 'Business::CyberSource::Request';
-with 'Business::CyberSource::Request::Role::DCC';
+with qw(
+	Business::CyberSource::Request::Role::DCC
+	Business::CyberSource::Request::Role::TaxService
+);
 
 use MooseX::Types::CyberSource qw( CaptureService );
 
