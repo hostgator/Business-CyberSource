3e1ed599c9c5ae745aa4a1b73e6e6fbec67003a4
Caleb Cushing <xenoterracide@gmail.com>
2012-10-09 11:25:38 -0500

    make role a stub for later removal
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Role/BillingInfo.pm
+++ lib/Business/CyberSource/Request/Role/BillingInfo.pm
@@ -6,28 +6,7 @@ use namespace::autoclean;
 # VERSION
 
 use Moose::Role;
-use MooseX::RemoteHelper;
-use MooseX::Aliases;
-
-use MooseX::Types::CyberSource qw( BillTo );
-
-use Class::Load qw( load_class );
-
-has bill_to => (
-	isa         => BillTo,
-	remote_name => 'billTo',
-	alias       => ['billing_info'],
-	is          => 'ro',
-	required    => 1,
-	coerce      => 1,
-);
 
 1;
 
 # ABSTRACT: Role for requests that require "bill to" information
-
-=attr bill_to
-
-L<Business::Cybersource::RequestPart::BillTo>
-
-=cut
