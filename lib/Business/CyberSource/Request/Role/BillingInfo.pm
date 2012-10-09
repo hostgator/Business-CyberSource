690d5d6d31db5c3667644eabe8891912879cb822
Caleb Cushing <xenoterracide@gmail.com>
2012-10-09 11:18:06 -0500

    remove more backcompat code
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Role/BillingInfo.pm
+++ lib/Business/CyberSource/Request/Role/BillingInfo.pm
@@ -13,63 +13,6 @@ use MooseX::Types::CyberSource qw( BillTo );
 
 use Class::Load qw( load_class );
 
-our @CARP_NOT = ( 'Class::MOP::Method::Wrapped', __PACKAGE__ );
-
-my %bt_map = (
-	first_name  => 1,
-	last_name   => 1,
-	city        => 1,
-	state       => 1,
-	postal_code => 1,
-	country     => 1,
-	email       => 1,
-	street1     => 1,
-	street2     => 1,
-	street3     => 1,
-	street4     => 1,
-	street      => 1,
-	ip          => 1,
-);
-
-around BUILDARGS => sub {
-	my $orig = shift;
-	my $self = shift;
-
-	my $args = $self->$orig( @_ );
-
-	return $args if defined $args->{bill_to};
-
-	load_class('Carp');
-	Carp::carp 'DEPRECATED: '
-		. 'pass a Business::CyberSource::RequestPart::BillTo to bill_to '
-		. 'or pass a constructor hashref to bill_to as it is coerced from '
-		. 'hashref.'
-		;
-
-	my %map = (
-		zip          => 'postal_code',
-		street       => 'street1',
-	);
-
-	my %newargs = map {(( $map{$_} || $_ ), $args->{$_})} keys %$args;
-
-	my %bill_to
-		= map {
-			defined $bt_map{$_} ? ( $_, delete $newargs{$_} ) : ()
-		} keys %newargs;
-
-	$newargs{bill_to}  = \%bill_to if keys %bill_to;
-
-	return \%newargs;
-};
-before [ keys %bt_map ] => sub {
-	load_class('Carp');
-	Carp::carp 'DEPRECATED: '
-		. 'call attribute methods ( ' . join( ' ', keys %bt_map ) . ' ) on '
-		. 'Business::CyberSource::RequestPart::BillTo via bill_to directly'
-		;
-};
-
 has bill_to => (
 	isa         => BillTo,
 	remote_name => 'billTo',
@@ -77,10 +20,6 @@ has bill_to => (
 	is          => 'ro',
 	required    => 1,
 	coerce      => 1,
-	handles     => {
-		street => 'street1',
-		%{ { map {( $_ => $_ )} keys %bt_map } }, ## no critic ( BuiltinFunctions::ProhibitVoidMap )
-	}
 );
 
 1;
