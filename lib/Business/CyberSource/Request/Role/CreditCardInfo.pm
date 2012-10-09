cbf8231915da8bf2667f3b834c55d8e41f6093ae
Caleb Cushing <xenoterracide@gmail.com>
2012-10-09 11:39:03 -0500

    remove CreditCardInfo backcompat
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Role/CreditCardInfo.pm
+++ lib/Business/CyberSource/Request/Role/CreditCardInfo.pm
@@ -10,83 +10,12 @@ use MooseX::RemoteHelper;
 
 use MooseX::Types::CyberSource qw( Card);
 
-use Class::Load qw( load_class );
-
-our @CARP_NOT = ( 'Class::MOP::Method::Wrapped', __PACKAGE__ );
-
-my %map = (
-	cc_exp_month => 'month',
-	cc_exp_year  => 'year',
-	exp_month    => 'month',
-	exp_year     => 'year',
-	cvn          => 'security_code',
-	csc          => 'security_code',
-	cvv2         => 'security_code',
-	cvc2         => 'security_code',
-	cid          => 'security_code',
-	credit_card  => 'account_number',
-	card_type    => 'card_type_code',
-);
-
-around BUILDARGS => sub {
-	my $orig = shift;
-	my $self = shift;
-
-	my $args = $self->$orig( @_ );
-
-	return $args if defined $args->{card};
-
-	load_class 'Carp';
-	Carp::carp 'DEPRECATED: '
-		. 'pass a Business::CyberSource::RequestPart::CreditCardInfo to '
-		. 'purchase_totals '
-		. 'or pass a constructor hashref to bill_to as it is coerced from '
-		. 'hashref.'
-		;
-
-	my %newargs = map {(( $map{$_} || $_ ), $args->{$_})} keys %$args;
-
-	my %cc_map = (
-		account_number => 1,
-		security_code  => 1,
-	);
-
-	my %ccexp_map = (
-		month => 1,
-		year  => 1,
-	);
-
-	my %card
-		= map {
-			defined $cc_map{$_} ? ( $_, delete $newargs{$_} ) : ()
-		} keys %newargs;
-
-	my %expiration
-		= map {
-			defined $ccexp_map{$_} ? ( $_, delete $newargs{$_} ) : ()
-		} keys %newargs;
-
-	$newargs{card           }  = \%card       if keys %card;
-	$newargs{card}{expiration} = \%expiration if keys %expiration;
-
-	return \%newargs;
-};
-
-before [ keys %map ] => sub {
-	load_class('Carp');
-	Carp::carp 'DEPRECATED: '
-		. 'call attribute methods ( ' . join( ' ', keys %map ) . ' ) on '
-		. 'Business::CyberSource::RequestPart::BillTo via bill_to directly'
-		;
-};
-
 has card => (
 	isa         => Card,
 	remote_name => 'card',
 	required    => 1,
 	is          => 'ro',
 	coerce      => 1,
-	handles     => \%map,
 );
 
 1;
