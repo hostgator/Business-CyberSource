5f8a71299b64e31c58369eccf69601c2556a02e1
Caleb Cushing <xenoterracide@gmail.com>
2012-10-15 17:35:56 -0500

    document purchasetotals
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/ResponsePart/PurchaseTotals.pm
+++ lib/Business/CyberSource/ResponsePart/PurchaseTotals.pm
@@ -15,3 +15,31 @@ with qw(
 __PACKAGE__->meta->make_immutable;
 1;
 # ABSTRACT: PurchaseTotals part of response
+
+=head1 EXTENDS
+
+L<Business::CyberSource::MessagePart>
+
+=head1 WITH
+
+=over
+
+=item L<Business::CyberSource::Role::Currency>
+
+=item L<Business::CyberSource::Role::ForeignCurrency>
+
+=back
+
+=attr currency
+
+=attr foreign_currency
+
+Billing currency returned by the DCC service. For the possible values, see the ISO currency codes
+
+=attr foreign_amount
+
+=attr exchange_rate
+
+=attr exchange_rate_timestamp
+
+=cut
