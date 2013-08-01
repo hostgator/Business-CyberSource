bdfe6523d41f089330c319ad5fc8c9769203d7b6
Caleb Cushing <xenoterracide@gmail.com>
2013-08-01 05:12:02 -0500

    remove deprecated alias for bill_to
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/CyberSource/Request/Role/BillingInfo.pm
+++ lib/Business/CyberSource/Request/Role/BillingInfo.pm
@@ -7,23 +7,16 @@ use Module::Load 'load';
 # VERSION
 
 use Moose::Role;
-use MooseX::Aliases;
 use MooseX::RemoteHelper;
 use MooseX::Types::CyberSource qw( BillTo );
 
 has bill_to => (
 	isa         => BillTo,
 	remote_name => 'billTo',
-	alias       => ['billing_info'],
 	is          => 'ro',
 	required    => 1,
 	coerce      => 1,
 );
-
-before billing_info => sub {
-	load Carp;
-	Carp::cluck 'DEPRECATED: call is_accept instead';
-};
 
 1;
 
