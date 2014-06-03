5e036ddf6be9c26ba07922ad8c1b94de416828ec
Robert Stone <robertstone@hostgator.com>
2014-06-03 14:06:40 -0500

    Corrected POD for ElectronicVerification
    Moved POD to bottom of file and corrected style of pod to be the same as
    the rest of the library.

--- lib/Business/CyberSource/Response/Role/ElectronicVerification.pm
+++ lib/Business/CyberSource/Response/Role/ElectronicVerification.pm
@@ -4,28 +4,103 @@ use strict;
 use warnings;
 
 # VERSION
-# ABSTRACT: Eletronic Verification Information
-
-=head1 NAME
-
-Business::CyberSource::Response::Role::ElectronicVerification
-
-=head1 SUMMARY
-
-When performing Authorization requests for American Express credit cards for some processors
-CyberSource will respond with Electronic Verification Information.  This Role implements support
-for them.
-
-=cut
 
 use Moose::Role;
 use MooseX::RemoteHelper;
 use MooseX::Types::CyberSource qw(ElectronicVerificationResult);
 
-=head1 ATTRIBUTES
+has ev_email => (
+    is           => 'ro',
+    isa          => ElectronicVerificationResult,
+    remote_name  => 'evEmail',
+    predicate    => 'has_ev_email',
+    required     => 0,
+);
+
+has ev_phone_number => (
+    is          => 'ro',
+    isa         => ElectronicVerificationResult,
+    remote_name => 'evPhoneNumber',
+    predicate   => 'has_ev_phone_number',
+    required    => 0,
+);
+
+has ev_postal_code => (
+    is          => 'ro',
+    isa         => ElectronicVerificationResult,
+    remote_name => 'evPostalCode',
+    predicate   => 'has_ev_postal_code',
+    required    => 0,
+);
+
+has ev_name => (
+    is          => 'ro',
+    isa         => ElectronicVerificationResult,
+    remote_name => 'evName',
+    predicate   => 'has_ev_name',
+    required    => 0,
+);
+
+has ev_street => (
+    is          => 'ro',
+    isa         => ElectronicVerificationResult,
+    remote_name => 'evStreet',
+    predicate   => 'has_ev_street',
+    required    => 0,
+);
+
+has ev_email_raw => (
+    is          => 'ro',
+    isa         => ElectronicVerificationResult,
+    remote_name => 'evEmailRaw',
+    predicate   => 'has_ev_email_raw',
+    required    => 0,
+);
+
+has ev_phone_number_raw => (
+    is          => 'ro',
+    isa         => ElectronicVerificationResult,
+    remote_name => 'evPhoneNumberRaw',
+    predicate   => 'has_ev_phone_number_raw',
+    required    => 0,
+);
+
+has ev_postal_code_raw => (
+    is          => 'ro',
+    isa         => ElectronicVerificationResult,
+    remote_name => 'evPostalCodeRaw',
+    predicate   => 'has_ev_postal_code_raw',
+    required    => 0,
+);
+
+has ev_name_raw => (
+    is          => 'ro',
+    isa         => ElectronicVerificationResult,
+    remote_name => 'evNameRaw',
+    predicate   => 'has_ev_name_raw',
+    required    => 0,
+);
+
+has ev_street_raw => (
+    is          => 'ro',
+    isa         => ElectronicVerificationResult,
+    remote_name => 'evStreetRaw',
+    predicate   => 'has_ev_street_raw',
+    required    => 0,
+);
+
+1;
+
+# ABSTRACT: Eletronic Verification Information
+
+=head1 DESCRIPTION
+
+When performing Authorization requests for American Express credit cards for some processors
+CyberSource will respond with Electronic Verification Information.  This Role implements support
+for them.
 
 All attributes have values according to CyberSource's documentation available at
-http://apps.cybersource.com/library/documentation/dev_guides/CC_Svcs_SO_API/Credit_Cards_SO_API.pdf
+L<http://apps.cybersource.com/library/documentation/dev_guides/CC_Svcs_SO_API/Credit_Cards_SO_API.pdf>
 
 There are two types of attributes, B<raw> are values specified by your payment processor while
 all other attributes are mapped by CyberSource from values specified by the payment processor.
@@ -66,124 +141,23 @@ The processor returned an unrecognized value.
 
 =back
 
-=head2 ev_email
+=attr ev_email
 
-=cut
+=attr ev_phone_number
 
-has ev_email => (
-    is           => 'ro',
-    isa          => ElectronicVerificationResult,
-    remote_name  => 'evEmail',
-    predicate    => 'has_ev_email',
-    required     => 0,
-);
+=attr ev_postal_code
 
-=head2 ev_phone_number
+=attr ev_name
 
-=cut
+=attr ev_street
 
-has ev_phone_number => (
-    is          => 'ro',
-    isa         => ElectronicVerificationResult,
-    remote_name => 'evPhoneNumber',
-    predicate   => 'has_ev_phone_number',
-    required    => 0,
-);
+=attr ev_email_raw
 
-=head2 ev_postal_code
+=attr ev_phone_number_raw
 
-=cut
+=attr ev_postal_code_raw
 
-has ev_postal_code => (
-    is          => 'ro',
-    isa         => ElectronicVerificationResult,
-    remote_name => 'evPostalCode',
-    predicate   => 'has_ev_postal_code',
-    required    => 0,
-);
+=attr ev_name_raw
 
-=head2 ev_name
+=attr ev_street_raw
 
-=cut
-
-has ev_name => (
-    is          => 'ro',
-    isa         => ElectronicVerificationResult,
-    remote_name => 'evName',
-    predicate   => 'has_ev_name',
-    required    => 0,
-);
-
-=head2 ev_street
-
-=cut
-
-has ev_street => (
-    is          => 'ro',
-    isa         => ElectronicVerificationResult,
-    remote_name => 'evStreet',
-    predicate   => 'has_ev_street',
-    required    => 0,
-);
-
-=head2 ev_email_raw
-
-=cut
-
-has ev_email_raw => (
-    is          => 'ro',
-    isa         => ElectronicVerificationResult,
-    remote_name => 'evEmailRaw',
-    predicate   => 'has_ev_email_raw',
-    required    => 0,
-);
-
-=head2 ev_phone_number_raw
-
-=cut
-
-has ev_phone_number_raw => (
-    is          => 'ro',
-    isa         => ElectronicVerificationResult,
-    remote_name => 'evPhoneNumberRaw',
-    predicate   => 'has_ev_phone_number_raw',
-    required    => 0,
-);
-
-=head2 ev_postal_code_raw
-
-=cut
-
-has ev_postal_code_raw => (
-    is          => 'ro',
-    isa         => ElectronicVerificationResult,
-    remote_name => 'evPostalCodeRaw',
-    predicate   => 'has_ev_postal_code_raw',
-    required    => 0,
-);
-
-=head2 ev_name_raw
-
-=cut
-
-has ev_name_raw => (
-    is          => 'ro',
-    isa         => ElectronicVerificationResult,
-    remote_name => 'evNameRaw',
-    predicate   => 'has_ev_name_raw',
-    required    => 0,
-);
-
-=head2 ev_street_raw
-
-=cut
-
-has ev_street_raw => (
-    is          => 'ro',
-    isa         => ElectronicVerificationResult,
-    remote_name => 'evStreetRaw',
-    predicate   => 'has_ev_street_raw',
-    required    => 0,
-);
-
-1;
