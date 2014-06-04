75d04e7f3238a9d7f4be968e9b3bd37faf1a4b7f
Robert Stone <robertstone@hostgator.com>
2014-06-04 04:04:11 -0500

    Implemented Support for new Exception Type based on Moose::Exception
    In Business::CyberSource:Exception
      Added attribute trace that isa XML::Compile::SOAP::Trace
      Added new method of stringification via overload of ""
        This new method will construct a string based representation of the
        XML::Compile::SOAP::Trace if such a trace exists or the string
        "Error" if there is not a helpful XML::Compile::SOAP::Trace

--- lib/Business/CyberSource/Exception.pm
+++ lib/Business/CyberSource/Exception.pm
@@ -7,6 +7,35 @@ use warnings;
 use Moose;
 extends 'Moose::Exception';
 
+has trace => (
+    is   => 'ro',
+    isa  => 'XML::Compile::SOAP::Trace',
+);
+
+use overload '""' =>
+    sub {
+        my $self = shift;
+
+        my $error;
+
+        if(defined $self->trace) {
+            $error = "Date: " . $self->trace->date     . "\n"
+                . "Request: "    . $self->trace->request->as_string  . "\n"
+                . "Response: "   . $self->trace->response->as_string . "\n";
+
+            if(defined $self->trace->error && length ($self->trace->error) ) {
+                $error .= "Error: " . $self->trace->error . "\n";
+            }
+        }
+        else {
+            $error = "Error";
+        }
+
+        return $error,
+    },
+    fallback => 1,
+;
+
 __PACKAGE__->meta->make_immutable;
 1;
 # ABSTRACT: base exception
