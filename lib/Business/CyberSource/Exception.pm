af50dcbcd6f8f8ca7f473bf24c58defb8f22cf60
Robert Stone <robertstone@hostgator.com>
2014-06-04 04:15:47 -0500

    Converted space indent to tab indent in Business::CyberSource::Exception

--- lib/Business/CyberSource/Exception.pm
+++ lib/Business/CyberSource/Exception.pm
@@ -8,34 +8,34 @@ use Moose;
 extends 'Moose::Exception';
 
 has trace => (
-    is        => 'ro',
-    isa       => 'Maybe[XML::Compile::SOAP::Trace]',
-    required  => 0,
-    predicate => 'has_trace',
+	is        => 'ro',
+	isa       => 'Maybe[XML::Compile::SOAP::Trace]',
+	required  => 0,
+	predicate => 'has_trace',
 );
 
 use overload '""' =>
-    sub {
-        my $self = shift;
+	sub {
+		my $self = shift;
 
-        my $error;
+		my $error;
 
-        if($self->has_trace && defined $self->trace) {
-            $error = "Date: " . $self->trace->date     . "\n"
-                . "Request: "    . $self->trace->request->as_string  . "\n"
-                . "Response: "   . $self->trace->response->as_string . "\n";
+		if($self->has_trace && defined $self->trace) {
+			$error = "Date: " . $self->trace->date     . "\n"
+				. "Request: "    . $self->trace->request->as_string  . "\n"
+				. "Response: "   . $self->trace->response->as_string . "\n";
 
-            if(defined $self->trace->error && length ($self->trace->error) ) {
-                $error .= "Error: " . $self->trace->error . "\n";
-            }
-        }
-        else {
-            $error = "Error";
-        }
+			if(defined $self->trace->error && length ($self->trace->error) ) {
+				$error .= "Error: " . $self->trace->error . "\n";
+			}
+		}
+		else {
+			$error = "Error";
+		}
 
-        return $error,
-    },
-    fallback => 1,
+		return $error,
+	},
+	fallback => 1,
 ;
 
 __PACKAGE__->meta->make_immutable;
